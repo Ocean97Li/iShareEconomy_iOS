a3ce9cb82dc8b27f0bda47ee21934f3dc05c301c
Ocean Li <ocean97li@gmail.com>
2019-11-10 10:18:43 +0100

    Initial Commit

---
+++ iShareEconomyApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iShareEconomyApp
+//
+//  Created by Hu Ocean Li on 10/11/2019.
+//  Copyright © 2019 Hu Ocean Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iShareEconomyApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iShareEconomyApp
+//
+//  Created by Hu Ocean Li on 10/11/2019.
+//  Copyright © 2019 Hu Ocean Li. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
