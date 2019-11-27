e979ef8f8c870028d895e8f1128b6845ef446fd2
Ocean Li <ocean97li@gmail.com>
2019-11-27 23:15:57 +0100

    Local Delete from Sharing Objects
    Only local

--- iShareEconomyApp/ViewControllers/TableViewControllers/LendObjectsTableViewController.swift
+++ iShareEconomyApp/ViewControllers/TableViewControllers/LendObjectsTableViewController.swift
@@ -70,7 +70,9 @@ class LendObjectsTableViewController: UITableViewController {
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
+        guard title == "Sharing" else {return false}
         return true
+        
     }
     
     func updateObjectItems(user: User) {
@@ -86,17 +88,13 @@ class LendObjectsTableViewController: UITableViewController {
         self.tableView.reloadData()
     }
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+
+    override func tableView(_ tableView:UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+          if editingStyle == UITableViewCell.EditingStyle.delete {
+              objects.remove(at: indexPath.row)
+              tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
     }
-    */
 
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
